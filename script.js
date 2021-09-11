0138f13dc21a5b8517c02121bb911d59e5e5395f
Konstantin_999 <sh-tov@yandex.ru>
2021-09-11 14:47:00 +0700

    fix: исправить замечания ревью

--- script.js
+++ script.js
@@ -14,23 +14,23 @@ const inputSubmitItemProfileInfo =
   popupProfileInfo.querySelector(".input-container");
 const inputSubmitItemElement = newCardPopup.querySelector(".input-container");
 const elements = document.querySelector(".elements");
-const elementsSectionTemplate =
+const elementSectionTemplate =
   document.querySelector("#elementsSection").content;
 setPopupCloseButtonEventListener(popupImg);
 setPopupCloseButtonEventListener(popupProfileInfo);
 setPopupCloseButtonEventListener(newCardPopup);
-function openPopap(popap) {
-  popap.classList.add("popup_opened");
-  popap.parentElement.classList.add("popup_opened");
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+  popup.parentElement.classList.add("popup_opened");
 }
 function closePopup(closePopup) {
   closePopup.parentElement.classList.add("popup_closed");
   closePopup.parentElement.classList.remove("popup_opened");
   closePopup.classList.remove("popup_opened");
 }
-function setPopupCloseButtonEventListener(card) {
-  card.querySelector(".popup__click").addEventListener("click", function (evt) {
-    closePopup(card.querySelector(".popup__click").parentElement);
+function setPopupCloseButtonEventListener(popup) {
+  popup.querySelector(".popup__click").addEventListener("click", function (evt) {
+    closePopup(popup);
   });
 }
 function setLikeButtonEventListener(card) {
@@ -47,35 +47,43 @@ function setRemoveCardEventListener(card) {
       evt.target.parentElement.remove();
     });
 }
-function addElementSection(link, name) {
+function createElementSection(card) {
   // функция возвращает карточку
-  const card = elementsSectionTemplate
+  const newCard = elementSectionTemplate
     .querySelector(".element")
     .cloneNode(true);
-  card.querySelector(".element__img").src = link;
-  card.querySelector(".element__figcaption").textContent = name;
+  newCard.querySelector(".element__img").src = card.link;
+  newCard.querySelector(".element__figcaption").textContent = card.name;
 
-  setLikeButtonEventListener(card);
-  setRemoveCardEventListener(card);
-  return card;
+  setLikeButtonEventListener(newCard);
+  setRemoveCardEventListener(newCard);
+  setImageClickEventListener(newCard, card);
+  return newCard;
 }
-function setImageClickEventListenere(elementSection, link, name) {
+function setImageClickEventListener(elementSection, card) {
   elementSection // вызов модального окна нажатием на картинку
     .querySelector(".element__img")
     .addEventListener("click", function () {
-      popupImg.querySelector(".img-popup").src = link;
-      popupImg.querySelector(".img-popup").alt = name;
-      popupImg.querySelector(".popup-figcaption").textContent = name;
-      openPopap(popupImg);
+      popupImg.querySelector(".img-popup").src = card.link;
+      popupImg.querySelector(".img-popup").alt = card.name;
+      popupImg.querySelector(".popup-figcaption").textContent = card.name;
+      openPopup(popupImg);
     });
 }
 profileInfoEditButton.addEventListener("click", function () {
-  openPopap(popupProfileInfo);
+  popupProfileInfo.querySelector(".input-container__item_name").value =
+  profile.querySelector(".profile-info__name").textContent;
+  popupProfileInfo.querySelector(".input-container__item_profession").value = 
+  profile.querySelector(".profile-info__vocation").textContent;
+  openPopup(popupProfileInfo);
 });
 profileAddButton.addEventListener("click", function () {
-  openPopap(newCardPopup);
+  newCardPopup.querySelector(".input-container__item_url").value = '';
+  newCardPopup.querySelector(
+    ".input-container__item_nameMesto"
+  ).value = '';
+  openPopup(newCardPopup);
 });
-
 inputSubmitItemProfileInfo.addEventListener("submit", function (evt) {
   evt.preventDefault();
   profile.querySelector(".profile-info__name").textContent =
@@ -83,50 +91,20 @@ inputSubmitItemProfileInfo.addEventListener("submit", function (evt) {
   profile.querySelector(".profile-info__vocation").textContent =
     popupProfileInfo.querySelector(".input-container__item_profession").value;
   closePopup(popupProfileInfo);
-  evt.target.reset(); // очистка формы
 });
 inputSubmitItemElement.addEventListener("submit", function (evt) {
   evt.preventDefault();
-  const link = newCardPopup.querySelector(".input-container__item_url").value;
-  const name = newCardPopup.querySelector(
-    ".input-container__item_nameMesto"
-  ).value;
-  const newcard = addElementSection(link, name);
-  elements.prepend(newcard); // добавляем карточку на страницу
-  setImageClickEventListenere(newcard, link, name);
+  const newCard = {
+      link: newCardPopup.querySelector(".input-container__item_url").value,
+      name: newCardPopup.querySelector(
+        ".input-container__item_nameMesto"
+      ).value
+    }
+  elements.prepend(createElementSection(newCard)); // добавляем карточку на страницу
   closePopup(newCardPopup);
   evt.target.reset(); // очистка формы
 });
 
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
-
 initialCards.forEach((card) => {
-  let itemOfBox = addElementSection(card.link, card.name);
-  setImageClickEventListenere(itemOfBox, card.link, card.name);
-  elements.prepend(itemOfBox);
-});
+  elements.append(createElementSection(card));
+})
